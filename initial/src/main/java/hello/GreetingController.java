30404687056928ee5c1e93934ee02d7843738da0
sgir <sathya.prabha.girish@gmail.com>
2018-02-09 11:47:58 -0500

    Add a GET endpoint

--- initial/src/main/java/hello/GreetingController.java
+++ initial/src/main/java/hello/GreetingController.java
@@ -2,16 +2,19 @@ package hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
 
+    private final static String templateString = "Hello, %S!";
+    private final AtomicLong counter = new AtomicLong();
+
     @RequestMapping(method= RequestMethod.GET, value = "/greeting")
-    public Greeting getGreeting(){
-
-        
-
-        return Greeting;
+    public Greeting getGreeting(@RequestParam(value = "name",defaultValue = "World") String name){
+        return new Greeting(counter.getAndIncrement(), String.format(templateString,name));
     }
 }
